3464722ca31ae3fecfe773eafbfc47b8dbabdede
AICALA\student <rafael>
2017-01-19 15:43:11 -0800

    DestructableBuildingScriptTest
    DestructableBuildingScriptTest

---
+++ Assets/ScriptLibrary/DistructBuilding.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistructBuilding : MonoBehaviour {
+    public float BuildingHealth = 0;
+    public GameObject Box; 
+    void Start ()
+    {
+        Box = this.gameObject;
+    }
+
+	void Update ()
+    {
+        if (BuildingHealth > 0)
+        {
+            RemoveRigidBody();
+        }
+        else
+        {
+            AddRigidBody();
+            this.transform.parent = null;
+        }
+    }
+    void AddRigidBody()
+    {
+        if(!Box.GetComponent<Rigidbody>())
+        {
+            Box.AddComponent<Rigidbody>();
+        }
+    }
+    void RemoveRigidBody()
+    {
+        if(Box.GetComponent<Rigidbody>())
+        {
+            Destroy(Box.GetComponent<Rigidbody>());
+        }
+    } 
+}
